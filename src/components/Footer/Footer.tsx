bc000576664c081a0b1cfcaae44e01265b9a4244
Alicia <a.skogblad@gmail.com>
2025-12-26 13:26:42 +0100

    :lipstick: update styling for footer & add navigation for links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,22 @@
+import { Link } from "react-router";
 import styles from "./Footer.module.scss"
 
 export const Footer = () => {
   return (
     <>
-      <div className={styles.Footer}>
-        <span>Kontakt</span>
-        <span>Vanliga frågor</span>
-        <span>Sociala medier</span>
-      </div>
+      <section className={styles.footerContainer}>
+        <div className={styles.logoSection}>
+          <img src="./logoLong.png" alt="logo" />
+          <p>Upptäck varma, inspirerande destinationer baserade på upplevelser, inte bara platser.</p>
+        </div>
+        
+        <div>
+          <h5>Länkar</h5>
+          <Link to="/search-destination">Upptäck</Link>
+          <Link to="/favorites">Favoriter</Link>
+          <Link to="/">Vanliga frågor</Link>
+        </div>
+      </section>
     </>
   );
 }
