e8aa68d80871cbfb63845c2980dc33b49e70cab9
Alicia <a.skogblad@gmail.com>
2025-12-29 18:03:59 +0100

    :lipstick: add new nav styling for search & favorite sites

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,26 +1,28 @@
-import { Link, NavLink } from "react-router"
+import { Link, NavLink, useLocation } from "react-router"
 import styles from "./Navigation.module.scss"
+import { Heart, Search } from "lucide-react";
 
 export const Navigation = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+  
   return (
-    <>
-      <nav className={styles.navigationContainer} aria-label="Huvudnavigation">
-        <Link to={"/"}>
-          <h1>Palm Tree Travels</h1>
-        </Link>
+    <nav className={`${styles.navigationContainer} ${isHomePage ? styles.navHomeContainer : ""}`} aria-label="Huvudnavigation">
+      <Link to={"/"}>
+        <h1>Palm Tree Travels</h1>
+      </Link>
 
-        <p>Upptäck varma, inspirerande destinationer baserade på upplevelser, inte bara platser</p>
+      <p>Upptäck varma, inspirerande destinationer baserade på upplevelser, inte bara platser</p>
 
-        <ul>
-          <li>
-            <NavLink to={"/search-destination"}>Sök destination</NavLink>
-          </li>
+      <ul>
+        <li>
+          <NavLink to={"/search-destination"}>{isHomePage ? "Sök destination" : <Search/>}</NavLink>
+        </li>
 
-          <li>
-            <NavLink to={"/favorites"}>Favoriter</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </>
+        <li>
+          <NavLink to={"/favorites"}>{isHomePage ? "Favoriter" : <Heart/>}</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 }
