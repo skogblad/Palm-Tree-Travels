fd081ea0f1ea147e7a0a6d39fc751e77f9d441d2
Alicia <a.skogblad@gmail.com>
2026-01-06 17:58:57 +0100

    :pencil2: fixed spelling error

--- src/services/serviceBase.ts
+++ src/services/serviceBase.ts
@@ -3,7 +3,7 @@
 export const getUrl = async <T> (url: string) => {
   const response = await fetch(url);
   if (!response.ok) {
-    throw new Error("Faild to fetch data");
+    throw new Error("Failed to fetch data");
   }
   return (await response.json()) as T;
 }
