140ace33c120a4982038c69b8db7694b668f8295
Alicia <a.skogblad@gmail.com>
2026-01-14 11:42:16 +0100

    :lipstick :art: Update component structure (add pageWrapper, reposition tags) and update styles

--- src/components/DestinationPresentation/DestinationPresentation.tsx
+++ src/components/DestinationPresentation/DestinationPresentation.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import type { WikipediaData } from "../../models/WikipediaData";
 import { getWikipediaData } from "../../services/wikipediaService";
 import { Link } from "react-router";
-import { MapPin } from "lucide-react";
+import { ArrowLeft, MapPin } from "lucide-react";
 import { useCurrentWeather } from "../../hooks/useCurrentWeather";
 
 type DestinationPresentationProps = {
@@ -26,56 +26,57 @@ export const DestinationPresentation = ({ destination }: DestinationPresentation
   }, [destination.wikiName, destination.name]);
 
   return (
-    <article key={destination.id} className={styles.destinationContainer}>
-      <div className={styles.textWrapper}>
-        <h2>{destination.name}</h2>
+    <>
+      <main className={styles.pageWrapper}>
+        <Link to={"/search-destination"} className={styles.goBackTextTop}><ArrowLeft/>Tillbaka</Link>
 
-        <div className={styles.countryTempWrapper}>
-          <span className={styles.countrySpan}><MapPin/>{destination.country}</span>
+        <article key={destination.id} className={styles.destinationContainer}>
+          <h2>{destination.name}</h2>
 
-          {isLoading && <span>Laddar väder...</span>}
-          {weather && (
-            <span className={styles.tempSpan}>
-              Aktuell temperatur:<img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="Väderikon" className={styles.weatherIcon}/>{Math.round(weather.temp)}°C
-            </span>
-          )}
-        </div>
-        
-        <p className={styles.pOne}>{destination.description}</p>
+          <div className={styles.countryTempWrapper}>
+            <span className={styles.countrySpan}><MapPin />{destination.country}</span>
+            {isLoading && <span>Laddar väder...</span>}
+            {weather && (
+              <span className={styles.tempSpan}>
+                Aktuell temperatur:<img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="Väderikon" className={styles.weatherIcon} />{Math.round(weather.temp)}°C
+              </span>
+            )}
+          </div>
 
-        {wikiText?.description.split('\n').map((paragraph, index) => (
-          <p key={index} className={styles.pTwo}>{paragraph}</p>
-        ))}
+          <div className={styles.textWrapper}>
+            <p className={styles.pOne}>{destination.description}</p>
+            {wikiText?.description.split('\n').map((paragraph, index) => (
+              <p key={index} className={styles.pTwo}>{paragraph}</p>
+            ))}
+            <p className={styles.readMoreTag}>
+              <a href={wikiText?.wikiUrl ?? undefined} target="_blank" rel="noopener noreferrer">Läs mer (länk)</a>
+            </p>
 
-        <p className={styles.readMoreTag}>
-          <a href={wikiText?.wikiUrl ?? undefined} target="_blank" rel="noopener noreferrer">Läs mer</a>
-        </p>
-        
-        <div className={styles.tempContainer}>
-          <span className={styles.monthlyTempTitle}>Genomsnittlig temperatur under året:</span>
-          {Object.entries(destination.avgTempByMonth).map(([month, temp]) => (
-            <div key={month} className={styles.tempMonthly}>
-              <span className={styles.month}>{month}: </span>
-              <span className={styles.temp}>{temp}°C</span>
+            <div className={styles.tempContainer}>
+              <span className={styles.monthlyTempTitle}>Genomsnittlig temperatur under året:</span>
+              {Object.entries(destination.avgTempByMonth).map(([month, temp]) => (
+                <div key={month} className={styles.tempMonthly}>
+                  <span className={styles.month}>{month}: </span>
+                  <span className={styles.temp}>{temp}°C</span>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-        
-        
-        <Link to={"/search-destination"} className={styles.backBtn}>Tillbaka till sökresultat</Link>
-      </div>
-      
-      <div className={styles.rightWrapper}>
-        <img src={destination.imageUrl} alt={destination.name} />
+          </div>
 
-        <div className={styles.experienceTags}>
-          {destination.experiences.map((experience, index) => (
-            <span key={index} className={styles.tag}>
-              {experience}
-            </span>
-          ))}
-        </div>
-      </div>
-    </article>
+          <div className={styles.rightWrapper}>
+            <img src={destination.imageUrl} alt={destination.name} />
+            <div className={styles.experienceTags}>
+              {destination.experiences.map((experience, index) => (
+                <span key={index} className={styles.tag}>
+                  {experience}
+                </span>
+              ))}
+            </div>
+          </div>
+        </article>
+
+        <Link to={"/search-destination"} className={styles.backBtn}>Tillbaka till sökresultat</Link>
+      </main>
+    </>
   )
 }
